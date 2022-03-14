b790a618a9d1bb1f5ab1ebff92ab349740377c9b
Matheus Ribeiro <matheusribeiro3105@gmail.com>
2022-03-13 21:58:09 -0300

    Props

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,23 +1,18 @@
 import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
+import { Movie } from "types/movie";
 
-function MovieCard() {
-  const movie = {
-    id: 1,
-    image:
-      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-    title: "The Witcher",
-    count: 2,
-    score: 4.5,
-  };
+type Props = {
+  movie: Movie;
+}
 
+
+
+function MovieCard({ movie } : Props) {
+  
   return (
     <div>
-      <img
-        className="dsmovie-movie-card-image"
-        src={movie.image}
-        alt={movie.title}
-      />
+      <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
       <div className="dsmovie-card-bottom-container">
         <h3>{movie.title}</h3>
         <MovieScore />
