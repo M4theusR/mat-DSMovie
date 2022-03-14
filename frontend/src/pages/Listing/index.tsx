65f44e1443ed21e800f4a0dc77ee1e9d0e029df8
Matheus Ribeiro <matheusribeiro3105@gmail.com>
2022-03-14 00:21:22 -0300

    UseParams

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import Pagination from "components/Pagination";
 import { BASE_URL } from "utils/requests";
 import { MoviePage } from "types/movie";
+
 function Listing() {
   const [pageNumber, setPageNumber] = useState(0);
 
