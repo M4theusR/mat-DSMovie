9f7b854a5ea47eb75c51fae2bc9ee8edb5722b96
Matheus Ribeiro <matheusribeiro3105@gmail.com>
2022-03-25 19:42:20 -0300

    Correção

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -8,10 +8,10 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/devsuperior">
+                    <a href="https://github.com/M4theusR">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/M4theusR</p>
                         </div>
                     </a>
                 </div>
